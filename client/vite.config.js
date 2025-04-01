b3f83beb9b87d5f0e0daf4ef5097cf0073bc9bd1
chandanbreddy <chandanbreddy@gmail.com>
2025-04-02 00:02:28 +0530

    Initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/register',  // Ensure correct base path
+  base: '/',  // Ensure correct base path
   server: {
     port: 5173,
   },
